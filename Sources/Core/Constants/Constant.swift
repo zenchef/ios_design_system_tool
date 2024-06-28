cd4a8d3515818d317c03e102b95205795dbc237c
Adriencullier <cullieradrien@gmail.com>
2024-06-28 16:31:39 +0200

    feat: change paths

--- Sources/Core/Constants/Constant.swift
+++ Sources/Core/Constants/Constant.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum K {
     static let assetPath = "../iOS-App/ZenKit/Sources/Assets/DSColors.xcassets"
-    static let primitivePath = "../iOS-App/ds-core-main/figma/primitive.json"
-    static let themeLightPath = "../iOS-App/ds-core-main/dist/ios/b2b-light/_core.json"
-    static let themeDarkPath = "../iOS-App/ds-core-main/dist/ios/b2b-dark/_core.json"
+    static let primitivePath = "../ds-core/figma/primitive.json"
+    static let themeLightPath = "../ds-core/dist/ios/b2b-light/_core.json"
+    static let themeDarkPath = "../ds-core/dist/ios/b2b-dark/_core.json"
 }
